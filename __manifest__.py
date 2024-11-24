9ed48fbfb624ae052976cf42ee91f4b52b424e18
cabdishakurr <cabdishakurrrr@gmail.com>
2024-11-24 15:17:39 +0300

    4
    4

--- __manifest__.py
+++ __manifest__.py
@@ -5,6 +5,7 @@
     'summary': 'Custom invoice type for hall rentals',
     'depends': ['account'],
     'data': [
+        'security/ir.model.access.csv',
         'views/account_move_views.xml',
     ],
     'installable': True,
